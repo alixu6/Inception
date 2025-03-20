e0e26f7d02484a254cd6ebf3e7d207e1d47b8ed4
alixu6 <alicezhx1@gmail.com>
2025-03-20 15:58:51 +0100

    clean up

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,7 +4,6 @@ mysqld_safe --user=mysql &
 sleep 5
 
 if [ ! -f "/var/lib/mysql/.initialized" ]; then
-    echo "initializing"
     mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
     mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
